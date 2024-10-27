7a6455233b1e27f983ca16ba285614775ffcf11f
Gatera <vgatera@gmail.com>
2024-10-27 15:04:00 +0200

    Comment

--- rwanda-currency-exchange/src/components/Footer.jsx
+++ rwanda-currency-exchange/src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <footer className='bg-blue-600 text-white py-6 h-52'>
         <div className='container mx-auto text-center'>
+          {/* Logo Image */}
             <img src="/media/RCE-Logo-white.png" alt="Rwanda Currency Exchange Logo" className='w-24 mx-auto mb-6 mt-2' />
         <p>&copy; 2024 RCE</p>
         </div>
